f69aaf84a8280568531777b9769280b7189b4dff
Suyog Magar <hkmagar11@gmail.com>
2025-06-25 23:58:23 +0530

    Initial commit

---
+++ src/main/java/com/stellarfs/bytechat/ByteChatApplication.java
@@ -0,0 +1,13 @@
+package com.stellarfs.bytechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ByteChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stellarfs/bytechat/ByteChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stellarfs.bytechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
